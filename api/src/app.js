68214cd6c0ac708bc396bc754fcc1e509ec6eae4
FacuCorona <coronafacundo@gmail.com>
2024-10-07 23:57:18 -0300

    endpoint /

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const { json } = require("body-parser");
+const { fetch } = require("cross-fetch");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', async (req, res, next) => {
+    try {
+        let getApiInfo = await fetch(`https://date.nager.at/api/v3/AvailableCountries`)
+            .then((resp) => resp.json())
+            .then((array) => {
+                console.log(array)
+                res.status(200).send(array)
+            })
+
+
+    } catch (error) {
+
+    }
 });
 
 module.exports = app;
