f4467f0b3e32f6e4419b66fb9a650d2024581d5b
FacuCorona <coronafacundo@gmail.com>
2024-10-10 03:07:25 -0300

    front-home

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const { json } = require("body-parser");
 const { fetch } = require("cross-fetch");
+const morgan = require('morgan');
+
 const app = express();
+
+app.name = 'API';
+
+// app.use(json.urlencoded({ extended: true, limit: '50mb' }));
+// app.use(json.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
+
+// Error catching endware.
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+});
+
+module.exports = server;
 
 app.get('/', async (req, res, next) => {
     try {
